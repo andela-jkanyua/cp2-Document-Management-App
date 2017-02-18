508187dfe733e08390ddd6f04a1d54eb69d689be
Jimnah Magira <jimnah.magira@andela.com>
2017-02-18 16:18:06 +0300

    [Feature#139444515] Add application entry point

--- server/server.js
+++ server/server.js
@@ -23,23 +23,12 @@ app.use(morgan('dev'));  // log every request to the console
 app.use(bodyParser.urlencoded({ extended: 'true' }));    // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());  // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
 // routes ======================================================================
-
+require('./routes/index')(app);
 // api ---------------------------------------------------------------------
-
-// return created index
-
-app.get('/users', (req, res) => {
-  res.json();
-});
-
-
-app.post('/users', (req, res) => {
-	res.status(400).send({
-		message: 'e.message',
-	});
-});
 
 // listen port 5000 on localhost(start app with node server.js) =======
 app.listen(process.env.PORT || 5000);
+console.log('Server on port 5000')
 module.exports = app
